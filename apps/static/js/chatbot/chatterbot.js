0bb4319f522d3f830433a1e16c2469f07f2a720d
Dharmendra <gohildharmendrab@gmail.com>
2022-10-15 15:17:28 +0530

    update color

--- apps/static/js/chatbot/chatterbot.js
+++ apps/static/js/chatbot/chatterbot.js
@@ -45,11 +45,11 @@ function ChattingData(text) {
     $('.chat-body').append('<div class="chat-bubble mt-1 me">' + text + '</div>').animate({ scrollTop: $('.chat-body').prop("scrollHeight") }, 400);
   }
   $('.chat-body').append(`<div class="divTyping chat-bubble you" id="divTyping"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto;display: block;shape-rendering: auto;width: 43px;height: 20px;" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-                <circle cx="0" cy="44.1678" r="15" fill="#ffffff">
+                <circle cx="0" cy="44.1678" r="15" fill="#000000">
                     <animate attributeName="cy" calcMode="spline" keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5" repeatCount="indefinite" values="57.5;42.5;57.5;57.5" keyTimes="0;0.3;0.6;1" dur="1s" begin="-0.6s"></animate>
-                </circle> <circle cx="45" cy="43.0965" r="15" fill="#ffffff">
+                </circle> <circle cx="45" cy="43.0965" r="15" fill="#000000">
                 <animate attributeName="cy" calcMode="spline" keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5" repeatCount="indefinite" values="57.5;42.5;57.5;57.5" keyTimes="0;0.3;0.6;1" dur="1s" begin="-0.39999999999999997s"></animate>
-            </circle> <circle cx="90" cy="52.0442" r="15" fill="#ffffff">
+            </circle> <circle cx="90" cy="52.0442" r="15" fill="#000000">
                 <animate attributeName="cy" calcMode="spline" keySplines="0 0.5 0.5 1;0.5 0 1 0.5;0.5 0.5 0.5 0.5" repeatCount="indefinite" values="57.5;42.5;57.5;57.5" keyTimes="0;0.3;0.6;1" dur="1s" begin="-0.19999999999999998s"></animate>
             </circle></svg></div>`);
   $('#txtmessage').val('');
