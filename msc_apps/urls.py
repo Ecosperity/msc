0a28b9fae3bbba2c4a8cca9e3395634e1a2ad48a
Ecosperity Admin <admin@ecosperityindia.com>
2022-11-14 18:41:26 +0530

    add

--- msc_apps/urls.py
+++ msc_apps/urls.py
@@ -14,7 +14,7 @@ if settings.DEBUG:
         path("400/", default_views.bad_request, kwargs={"exception": Exception("Bad Request!")},),
         path("403/", default_views.permission_denied, kwargs={"exception": Exception("Permission Denied")},),
         path("404/", default_views.page_not_found, kwargs={"exception": Exception("Page not Found")},),
-        path("500/", default_views.server_error),
+        # path("500/", default_views.server_error),
     ]
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
